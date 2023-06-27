ae0d3953f4fd8ecb75dda548d99c66cadefd1619
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-06-27 19:54:24 +0200

    feat: add wildcard redirect

--- dns/dns.tf
+++ dns/dns.tf
@@ -8,7 +8,7 @@ resource "pihole_dns_record" "host_records" {
   ip       = each.value
 }
 
-resource "pihole_cname_record" "record" {
+resource "pihole_cname_record" "pihole_records_internal" {
   for_each = toset(var.records_internal)
   domain   = format("%s.%s", each.key, var.domain_name)
   target   = format("%s.%s", var.lb_host, var.domain_name)
@@ -16,10 +16,20 @@ resource "pihole_cname_record" "record" {
 
 # Cloudflare
 
-resource "cloudflare_record" "example" {
+resource "cloudflare_record" "cloudflare_records_internal" {
   for_each = toset(var.records_internal)
   zone_id  = var.cloudflare_zone_id
   name     = each.value
+  value    = format("%s.%s", var.lb_host, var.tailscale_domain)
+  type     = "CNAME"
+  ttl      = 3600
+  proxied  = false
+}
+
+# wildcard is not supported by pihole API, so we manage it separately
+resource "cloudflare_record" "wildcard_record" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "*"
   value    = format("%s.%s", var.lb_host, var.tailscale_domain)
   type     = "CNAME"
   ttl      = 3600
