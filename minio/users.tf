55212c9d8bc496396f9316209e7ffcb6e416d61d
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-11-04 10:20:21 +0100

    fix(minio): minio users

--- minio/users.tf
+++ minio/users.tf
@@ -15,4 +15,5 @@ resource "minio_iam_user_policy_attachment" "developer" {
   for_each    = toset(var.users)
   user_name   = minio_iam_user.users[each.key].id
   policy_name = minio_iam_policy.policies[each.key].id
+  depends_on  = [minio_iam_policy.policies, minio_iam_user.users]
 }
